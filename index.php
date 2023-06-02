a521f33e807aedc638b714cb0653b15c30ded057
JuanCamiloPaezArizaCAMPUS <camipa2305@gmail.com>
2023-06-02 16:29:55 -0500

    feat: :sparkles: Punto 4 taller
    Terminado punto 4 taller arrays

--- index.php
+++ index.php
@@ -1,19 +1,34 @@
 <?php
  /**
-  *todo: Tercer punto taller arrays 
-  **Filtrar planetas habitables
+  *todo: Cuarto punto taller arrays 
+  **Calcular la gravedad en diferentes planetas
   */
+$gravedadR = array(
+    "Mercurio" => 0.38,
+    "Venus" => 0.91,
+    "Tierra" => 1.00,
+    "Marte" => 0.38,
+    "Jupiter" => 2.53,
+    "Saturno" =>  1.07,
+    "Urano" => 0.92,
+    "Neptuno" => 1.19
+);
+function calcularGravedad($Grelativa){
+    $Greal = array();
+    $Gtierra = 9.8;
 
-$planetas = array('Sol'=>false,'Mercury'=>false,'Venus'=>false,'Earth'=>true,'Mars'=>true,'Jupiter'=>false,'Saturn'=>false,'Uranus'=>false,'Neptune'=>false);
-
-$filtrados= array_filter($planetas,function($habitable){
-    return $habitable;
-});
-
-echo "Planetas habitables: <br>";
-foreach($filtrados as $planetas => $habitable){
-    echo "$planetas <br>";
+    foreach($Grelativa as $planeta => $relativa){
+        $Greal[$planeta] = $relativa * $Gtierra;
+    }
+    return $Greal;
 }
+
+$Greal = calcularGravedad($gravedadR);
+echo "Gravedad aprox de los planetas: <br>";
+foreach($Greal as $planeta => $gravedad){
+    echo "$planeta:$gravedad m/s^2 <br>";
+}
+
 
 ?>
  
