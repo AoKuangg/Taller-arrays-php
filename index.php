5075d22d99e67793c7c22e98df0b6174b0e4b2c1
JuanCamiloPaezArizaCAMPUS <camipa2305@gmail.com>
2023-06-05 16:09:35 -0500

    feat: :sparkles: Punto 12 taller
    Terminado punto 12 taller arrays

--- index.php
+++ index.php
@@ -1,35 +1,49 @@
 <?php
  /**
-  *todo: Onceavo punto taller arrays 
-  **Encontrar planetas únicos en dos sistemas solares
+  *todo: Doceavo punto taller arrays 
+  **Crear una lista de satélites para un planeta
   */
 
-  if (isset($_POST['sistema1']) && isset($_POST['sistema2'])) {
-    $textoSistema1 = $_POST['sistema1'];
-    $textoSistema2 = $_POST['sistema2'];
+  if (isset($_POST['planeta']) && isset($_POST['satelites'])) {
+    $planeta = $_POST['planeta'];
+    $satelites = $_POST['satelites'];
 
-    $sistema1 = explode("\n", $textoSistema1);
-    $sistema2 = explode("\n", $textoSistema2);
+    $arraySatelites = explode("\n", $satelites);
 
-    $planetasUnicosSistema1 = array_diff($sistema1, $sistema2);
-    $planetasUnicosSistema2 = array_diff($sistema2, $sistema1);
+    $listaSatelites = [];
+    if (file_exists('satelites.txt')) {
+        $listaSatelites = unserialize(file_get_contents('satelites.txt'));
+    }
 
-    $textoPlanetasUnicosSistema1 = implode("\n", $planetasUnicosSistema1);
-    $textoPlanetasUnicosSistema2 = implode("\n", $planetasUnicosSistema2);
+    $listaSatelites[$planeta] = array_merge($listaSatelites[$planeta] ?? [], $arraySatelites);
+
+    file_put_contents('satelites.txt', serialize($listaSatelites));
 }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Planetas únicos</title>
+    <title>Rastreador de satélites</title>
 </head>
 <body>
-    <h3>Planetas únicos</h3>
-    <p>Lista de planetas únicos en cada sistema solar:</p>
-    <h2>Sistema solar 1:</h2>
-    <textarea rows="5" cols="30" readonly><?php echo $textoPlanetasUnicosSistema1; ?></textarea>
-    <h2>Sistema solar 2:</h2>
-    <textarea rows="5" cols="30" readonly><?php echo $textoPlanetasUnicosSistema2; ?></textarea>
+    <h1>Rastreador de satélites</h1>
+    <?php
+
+    if (isset($_POST['planeta'])) {
+        $planetaSeleccionado = $_POST['planeta'];
+        echo "<h2>Satélites de $planetaSeleccionado:</h2>";
+        
+        if (isset($listaSatelites[$planetaSeleccionado])) {
+            $satelitesPlaneta = $listaSatelites[$planetaSeleccionado];
+            foreach ($satelitesPlaneta as $satelite) {
+                echo "$satelite<br>";
+            }
+        } else {
+            echo "No se encontraron satélites para $planetaSeleccionado.";
+        }
+    }
+    ?>
 </body>
 </html>
+ 
