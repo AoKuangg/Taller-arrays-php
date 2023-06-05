c71112fb5302c5170d3624265ad79311285da3b1
JuanCamiloPaezArizaCAMPUS <camipa2305@gmail.com>
2023-06-05 15:55:10 -0500

    feat: :sparkles: Punto 11 taller
    Terminado punto 11 taller

--- index.php
+++ index.php
@@ -1,34 +1,35 @@
 <?php
  /**
-  *todo: Decimo punto taller arrays 
-  **Encontrar planetas comunes en dos sistemas solares
+  *todo: Onceavo punto taller arrays 
+  **Encontrar planetas únicos en dos sistemas solares
   */
 
   if (isset($_POST['sistema1']) && isset($_POST['sistema2'])) {
-
     $textoSistema1 = $_POST['sistema1'];
     $textoSistema2 = $_POST['sistema2'];
-
 
     $sistema1 = explode("\n", $textoSistema1);
     $sistema2 = explode("\n", $textoSistema2);
 
+    $planetasUnicosSistema1 = array_diff($sistema1, $sistema2);
+    $planetasUnicosSistema2 = array_diff($sistema2, $sistema1);
 
-    $planetasComunes = array_intersect($sistema1, $sistema2);
-
-    $textoPlanetasComunes = implode("\n", $planetasComunes);
+    $textoPlanetasUnicosSistema1 = implode("\n", $planetasUnicosSistema1);
+    $textoPlanetasUnicosSistema2 = implode("\n", $planetasUnicosSistema2);
 }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Planetas comunes</title>
+    <title>Planetas únicos</title>
 </head>
 <body>
-    <h1>Planetas comunes</h1>
-    <p>Lista de planetas comunes en los dos sistemas solares:</p>
-    <textarea rows="5" cols="30" readonly><?php echo $textoPlanetasComunes; ?></textarea>
+    <h3>Planetas únicos</h3>
+    <p>Lista de planetas únicos en cada sistema solar:</p>
+    <h2>Sistema solar 1:</h2>
+    <textarea rows="5" cols="30" readonly><?php echo $textoPlanetasUnicosSistema1; ?></textarea>
+    <h2>Sistema solar 2:</h2>
+    <textarea rows="5" cols="30" readonly><?php echo $textoPlanetasUnicosSistema2; ?></textarea>
 </body>
 </html>
- 
